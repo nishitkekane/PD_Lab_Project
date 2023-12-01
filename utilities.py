d85b91e2f39f52a81e6933d1576e35c27dd5f382
manascb1344 <zephop76593@gmail.com>
2023-12-01 18:49:42 +0530

    Modified load path file compatibility

--- utilities.py
+++ utilities.py
@@ -1,15 +1,28 @@
 import pygame as pg
 import sys
+import os
 
 # Initialize Font
 pg.font.init()
 pg.mixer.init()
 
+
+# Resource Path Compatibility
+def resource_path(relative_path):
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
 # Font
-font = pg.font.Font("Assets/Fonts/Valorax.otf", 44)
-font_small = pg.font.Font("Assets/Fonts/Valorax.otf", 25)
-main_font = pg.font.Font("Assets/Fonts/Alpharush.ttf", 25)
-lost_font = pg.font.Font("Assets/Fonts/Alpharush.ttf", 50)
+font = pg.font.Font(resource_path("Assets/Fonts/Valorax.otf"), 44)
+font_small = pg.font.Font(resource_path("Assets/Fonts/Valorax.otf"), 25)
+main_font = pg.font.Font(resource_path("Assets/Fonts/Alpharush.ttf"), 25)
+lost_font = pg.font.Font(resource_path("Assets/Fonts/Alpharush.ttf"), 50)
 
 # Window
 width, height = 750, 750
@@ -17,20 +30,21 @@ win = pg.display.set_mode((width, height))
 
 # Background
 bg = pg.transform.scale(
-    pg.image.load("Assets/Images/background-black.png"), (width, height)
+    pg.image.load(resource_path("Assets/Images/background-black.png")), (width, height)
 )
 
 
 # Load additional backgrounds
 backgrounds = {
     "Default": pg.transform.scale(
-        pg.image.load("Assets/Images/background-black.png"), (width, height)
+        pg.image.load(resource_path("Assets/Images/background-black.png")),
+        (width, height),
     ),
     "Background1": pg.transform.scale(
-        pg.image.load("Assets/Images/bg_01.png"), (width, height)
+        pg.image.load(resource_path("Assets/Images/bg_01.png")), (width, height)
     ),
     "Background2": pg.transform.scale(
-        pg.image.load("Assets/Images/bg_02.png"), (width, height)
+        pg.image.load(resource_path("Assets/Images/bg_02.png")), (width, height)
     ),
 }
 
@@ -56,35 +70,35 @@ def drawText(text, font, color, surface, x, y):
 
 # Load images
 # ships
-red_ship = pg.image.load("Assets/Images/red_ship.png")
+red_ship = pg.image.load(resource_path("Assets/Images/red_ship.png"))
 red_ship = pg.transform.scale(
     red_ship, (red_ship.get_width() * 0.6, red_ship.get_height() * 0.6)
 )
 
-blue_ship = pg.image.load("Assets/Images/blue_ship.png")
+blue_ship = pg.image.load(resource_path("Assets/Images/blue_ship.png"))
 blue_ship = pg.transform.scale(
     blue_ship, (blue_ship.get_width() * 0.6, blue_ship.get_height() * 0.6)
 )
 
-green_ship = pg.image.load("Assets/Images/green_ship.png")
+green_ship = pg.image.load(resource_path("Assets/Images/green_ship.png"))
 green_ship = pg.transform.scale(
     green_ship, (green_ship.get_width() * 0.6, green_ship.get_height() * 0.6)
 )
-yellow_ship = pg.image.load("Assets/Images/yellow_ship.png")
+yellow_ship = pg.image.load(resource_path("Assets/Images/yellow_ship.png"))
 yellow_ship = pg.transform.scale(
     yellow_ship, (yellow_ship.get_width() * 0.9, yellow_ship.get_height() * 0.9)
 )
 # lasers
-red_laser = pg.image.load("Assets/Images/pixel_laser_red.png")
-yellow_laser = pg.image.load("Assets/Images/pixel_laser_yellow.png")
-green_laser = pg.image.load("Assets/Images/pixel_laser_green.png")
-blue_laser = pg.image.load("Assets/Images/pixel_laser_blue.png")
+red_laser = pg.image.load(resource_path("Assets/Images/pixel_laser_red.png"))
+yellow_laser = pg.image.load(resource_path("Assets/Images/pixel_laser_yellow.png"))
+green_laser = pg.image.load(resource_path("Assets/Images/pixel_laser_green.png"))
+blue_laser = pg.image.load(resource_path("Assets/Images/pixel_laser_blue.png"))
 
 # Load Audio files
-buttonAudio = pg.mixer.Sound("Assets/Audio/Button.wav")
-explosionAudio = pg.mixer.Sound("Assets/Audio/Explosion.mp3")
-gunshotAudio = pg.mixer.Sound("Assets/Audio/LaserShot.mp3")
-backgroundAudio = pg.mixer.Sound("Assets/Audio/BackgroundMusic.mp3")
+buttonAudio = pg.mixer.Sound(resource_path("Assets/Audio/Button.wav"))
+explosionAudio = pg.mixer.Sound(resource_path("Assets/Audio/Explosion.mp3"))
+gunshotAudio = pg.mixer.Sound(resource_path("Assets/Audio/LaserShot.mp3"))
+backgroundAudio = pg.mixer.Sound(resource_path("Assets/Audio/BackgroundMusic.mp3"))
 
 
 # Game Functions
